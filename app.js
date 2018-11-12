ff8683158e1a0be83217534821547917ee0b6563
nahidul333 <44962482+nahidul333@users.noreply.github.com>
2018-11-12 16:41:06 +0900

    initial comment

---
+++ app.js
@@ -0,0 +1,20 @@
+// The large image
+let largeImg = document.getElementById('current'); 
+
+const allImgsEsy = document.querySelectoryAll('.imgs img');
+
+for(let counter =0; counter <= 7; counter = 
+counter+1){
+allImgsEasysy[counter]
+.addEventlistener('click',function(event){
+  current.src = event.target.src;placeName.innerHTML = event.target.alt;
+})
+}
+
+
+
+
+
+
+
+
